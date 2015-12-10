443c64b413c7dfdddc88425efb5f0bcd00bec255
ushiboy <ushiboy.dev@gmail.com>
2015-12-10 23:06:01 +0900

    add errors

--- error_behaviors/index.php
+++ error_behaviors/index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
 set_error_handler(function($severity, $message, $file, $line) {
@@ -7,9 +9,29 @@ set_error_handler(function($severity, $message, $file, $line) {
 
 try {
     $handle = fopen('none', 'r');
+    print("don't reach\n");
+} catch (Error $e) {
+    print($e->getMessage()."\n");
+} catch (ErrorException $e) {
+    print($e->getMessage()."\n");  // <- catch!
+}
+
+try {
+    none_function();
     print("don't reach");
 } catch (Error $e) {
-    print($e);
+    print($e->getMessage()."\n");  // <- catch!
 } catch (ErrorException $e) {
-    print($e);
+    print($e->getMessage()."\n");
+}
+
+try {
+    $result = json_decode('invalid json string');
+    if (json_last_error() !== JSON_ERROR_NONE) {
+      print("json decode error\n");
+    }
+} catch (Error $e) {
+    print($e->getMessage()."\n");
+} catch (ErrorException $e) {
+    print($e->getMessage()."\n");
 }
